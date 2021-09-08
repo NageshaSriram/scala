710ca414b15da6a01bd452209e508e05ad0d2dd3
Nagesha S <43410978+NageshaSriram@users.noreply.github.com>
2021-09-09 00:27:56 +0530

    Scala Project setup and First Hello World Program and run

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "learn-scala"
+
+version := "0.1"
+
+scalaVersion := "3.0.2"
+
+idePackagePrefix := Some("org.learnscala.application")
---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,5 @@
+package org.learnscala.application
+
+object HelloWorld extends App {
+  println("Hello world, I am coming soon to conquere you..!!")
+}
